8693c6fc1a7d1d59c324d720790dc9aabf49206d
Clpr <tianhao.zhao@emory.edu>
2021-07-28 15:46:25 -0400

    refactor proj

--- pages/DrZhongCollectData/deprecated/generate_htmlpage.jl
+++
@@ -1,9 +0,0 @@
-    push!(LOAD_PATH,pwd())
-    using HtmlConstructor
-
-
-# ------------------- 初始化一个页面实例
-page = HtmlPage(filename = "NewProfile.html")
-
-# ------------------- 填充头部
-# tmp_head = 
